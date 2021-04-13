622d0ae8e152c37b140de760d26db7c171ddc7a1
Arkadiusz Zygan <a.zygan@gmail.com>
2021-04-13 20:15:06 +0200

    Edit folder structure

---
+++ src/views/jobs/Jobs.vue
@@ -0,0 +1,23 @@
+<template>
+  <h1>Jobs</h1>
+  <div v-for="job in jobs" :key="job.id">
+    <h3>{{ job.title }}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      jobs: [
+        { title: "Ux Designer", id: 1, details: "lorem" },
+        { title: "Web Developer", id: 2, details: "lorem" },
+        { title: "Vue Developer", id: 3, details: "lorem" },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+</style>
